f618eb564529d9827a5334a698e2c9b89f5adc2e
nouraalfr <nouraalfraidi90@gmail.com>
2023-02-04 21:29:10 +0300

    added likes and dislikes for details page

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,11 @@ const client = new MongoClient('mongodb://127.0.0.1');
 const db = client.db('app');
 const coll = db.collection('places');
 
+app.use(express.json());
+app.use(express.static('..'));
+
 // Get cafes and restaurants
-app.get('/', async (req, res) => {
-  // Allow all cross origin
-  res.header('Access-Control-Allow-Origin', '*');
+app.get('/api', async (req, res) => {
   // If request has query then use it to filter results
   const filter = {};
   if (req.query.type) {
@@ -20,31 +21,50 @@ app.get('/', async (req, res) => {
   if (req.query.query) {
     filter.$text = { $search: req.query.query };
   }
-  if (req.query.rating) {
-    filter.rating = Number(req.query.rating);
+  // Set sorting
+  let sort = { created_at: -1 };
+  if (req.query.sort === 'top') {
+    sort = { likes: -1 };
   }
   // Set limit
   let limit = 0;
   if (req.query.limit) {
     limit = Number(req.query.limit);
   }
-  // Always sort by latest
-  const cursor = coll.find(filter).sort({ created_at: -1 }).limit(limit);
+  const cursor = coll.find(filter).sort(sort).limit(limit);
   // Get results and return to user
   const results = await cursor.toArray();
   res.send(results);
 });
 
+app.post('/api/rate', async (req, res) => {
+  try {
+    const placeName = req.body.name;
+    const like = req.body.like === true;
+    console.log(like);
+    if (like) {
+      await coll.updateOne({ name: placeName }, { $inc: { likes: 1 } });
+    } else {
+      await coll.updateOne({ name: placeName }, { $inc: { likes: -1 } });
+    }
+  } catch {
+    res.send('invalid request');
+  }
+  res.send('success');
+});
+
 app.listen(port, async () => {
   // delete database at start
-  //await coll.drop();
+  try {
+    await coll.drop();
+  } catch {}
   // create index for name field so we can search
   await coll.createIndex({ name: 'text' });
   await insertItems();
   console.log(`Backend listening on port ${port}`);
 });
 
-async function insertItems() {
+async function insertItems () {
   // Read data from file
   const data = JSON.parse(fs.readFileSync('data.json'));
   // Insert data into mongodb
